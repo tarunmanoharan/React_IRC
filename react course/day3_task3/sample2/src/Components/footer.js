16f5c2d4bdb27a2d3b1ccae4595a90d9bda73fd7
tarunmanoharan <98203435+tarunmanoharan@users.noreply.github.com>
2023-12-12 00:37:14 +0530

    gallery and footer added

--- react course/day3_task3/sample2/src/Components/footer.js
+++ react course/day3_task3/sample2/src/Components/footer.js
@@ -1,15 +1,21 @@
+import React from 'react';
+import '../assets/css/footor.css';
 
-function Footer()
-{
-    return(
-        
-        <div>
-
-                <h3>CopyRight Issues</h3>
-
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p>Explore the Classic Mustang 1969 - A Ford Legend</p>
+        <div className="keywords">
+          <span>Classic Cars</span>
+          <span>Site Feedback</span>
+          <span>Contact Us</span>
+          <span>Terms & Conditions</span>
+          <span>Mustang 1969</span>
         </div>
-    );
-    
-}
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
